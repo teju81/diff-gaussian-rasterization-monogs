5ee9c9ddd9b7f61bc105bc99f187940da1431fbc
teju81 <teju81@gmail.com>
2025-01-08 17:37:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=['diff_gaussian_rasterization_monogs'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="diff_gaussian_rasterization_monogs._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
