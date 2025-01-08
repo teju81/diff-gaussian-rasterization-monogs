ff7862fed49a9f78744bd7b238c5571b8110cf98
teju81 <teju81@gmail.com>
2025-01-08 15:14:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="diff_gaussian_rasterization_monogs",
+    packages=['diff_gaussian_rasterization_monogs'],
     ext_modules=[
         CUDAExtension(
             name="diff_gaussian_rasterization._C",
